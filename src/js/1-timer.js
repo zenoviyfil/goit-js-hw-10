8621cf0325119c6fe7f398a6834e7e555f8e78dc
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-01-09 15:31:20 +0100

    NIC

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,14 +1,26 @@
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
+// const startBtn = document.querySelector('[data-start]')
+// startBtn.addEventListener('click', )
+
+let userSelectedDate = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    userSelectedDate = selectedDates;
+    if(this.defaultDate < userSelectedDate) {
+        return alert('Please choose a date in the future');
+    } else {
+        
+    }
     console.log(selectedDates[0]);
   },
 };
+
 
 flatpickr('#datetime-picker', {options});
