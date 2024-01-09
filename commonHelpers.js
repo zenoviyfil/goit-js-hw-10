4e2ef5fceadea749e71f7718062d1d206be5e1ae
zenoviyfil <98259328+zenoviyfil@users.noreply.github.com>
2024-01-09 14:31:42 +0000

    Deploying to gh-pages from @ zenoviyfil/goit-js-hw-10@8621cf0325119c6fe7f398a6834e7e555f8e78dc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as t}from"./assets/vendor-992cd48f.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",{options:o});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as r}from"./assets/vendor-992cd48f.js";let t=0;const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){if(t=e,this.defaultDate<t)return alert("Please choose a date in the future");console.log(e[0])}};r("#datetime-picker",{options:o});
 //# sourceMappingURL=commonHelpers.js.map
