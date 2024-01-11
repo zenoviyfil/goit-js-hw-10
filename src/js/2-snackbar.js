0363cd5ea3ad7720e6d8f9b3d4a57ea7cb98f3a4
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-01-11 13:34:15 +0100

    snackbar half way

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,34 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', createPromise);
+
+function createPromise() {
+            const fullfieldStatus = form.elements.state.value;
+            const delay = form.elements.delay.value;
+
+            console.log(fullfieldStatus)
+            console.log(delay)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (fullfieldStatus === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    })
+      .then(delay =>
+        iziToast.info({ message: `✅ Fulfilled promise in ${delay}ms` })
+      )
+      .catch(delay =>
+        iziToast.info({ message: `❌ Rejected promise in ${delay}ms` })
+      );
+};
+
+// console.log(form);
+// console.log(delay);
+// console.log(fullfieldStatus);
+// console.log(rejectedStatus);
